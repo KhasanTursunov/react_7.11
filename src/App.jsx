301c68b91c63ef7dab52e8e44bb2380da9518748
KhasanTursunov <khasantursunovrustamovich@gmail.com>
2025-03-03 11:13:27 +0500

    fixed some problems with vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import Products from "./components/products";
 import Hero from "./components/Hero";
-import Header from "./components/Header";
 
 
 const App = () => {
   return (
     <div>
-      {/* <Header/> */}
       <Hero/>
     </div>
   );
